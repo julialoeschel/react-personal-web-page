c0e0987ddbe79cce04c2cecd82a6cddeb7114bb0
julialoeschel <juloeschel@gmail.com>
2022-01-09 15:52:50 +0100

    fix mudules.css pt 2

--- src/app/components/ContainerYellow/ContainerYellow.tsx
+++ src/app/components/ContainerYellow/ContainerYellow.tsx
@@ -1,7 +1,7 @@
-import classes from './';
+import classes from './ContainerYellow.modules.css';
 
 function ContainerYellow(): JSX.Element {
-  return <div className={classes.conatiner}>green Container</div>;
+  return <div>green Container</div>;
 }
 
 export default ContainerYellow;

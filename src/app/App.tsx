b50c379ea1e2347936d7739dc310972b1578d5fb
julialoeschel <juloeschel@gmail.com>
2022-01-09 15:50:35 +0100

    fix modules.css pt 1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
+import Button from './components/Button/Button';
+import ContainerYellow from './components/ContainerYellow/ContainerYellow.jsx';
+
 function App(): JSX.Element {
   return (
     <>
       <p> hallo - this is some content here</p>
+      <Button>Press </Button>
+      <ContainerYellow></ContainerYellow>
     </>
   );
 }
---
+++ src/app/components/ContainerYellow/ContainerYellow.tsx
@@ -0,0 +1,7 @@
+import classes from './';
+
+function ContainerYellow(): JSX.Element {
+  return <div className={classes.conatiner}>green Container</div>;
+}
+
+export default ContainerYellow;
